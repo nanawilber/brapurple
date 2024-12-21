074f4eb32b5bdca3a20556961dbfdd612f6fec85
uniquetheo <55855459+uniquetheo@users.noreply.github.com>
2024-12-21 09:54:06 +0000

    feat: Nav backdrop

--- components/NavSection.jsx
+++ components/NavSection.jsx
@@ -3,9 +3,9 @@ import { Button } from "./ui/button";
 
 const NavSection = () => {
   return (
-    <div className="bg-purple-600 sticky top-0 z-50 shadow-md">
+    <div className="dark:bg-black/50 backdrop-blur-sm sticky top-0 z-50">
       <div className="h-16 container mx-auto px-4 flex justify-between items-center">
-        <div className=" font-bold text-xl">Brapurple</div>
+        <div className=" font-bold text-xl text-primary">Brapurple</div>
         <nav className="hidden md:flex space-x-4 md:space-x-8 lg:space-x-12">
           <div className="">Home</div>
           <div className="">About</div>
