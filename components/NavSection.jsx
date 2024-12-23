83fd42709d030e6f902945bd0168d8e959d4f0c1
uniquetheo <55855459+uniquetheo@users.noreply.github.com>
2024-12-23 01:02:20 +0000

    feat: mobile menu

--- components/NavSection.jsx
+++ components/NavSection.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { AlignJustify, X } from "lucide-react";
 
 const NavLinks = [
   { name: "Home", slug: "home", path: "/" },
@@ -9,9 +12,15 @@ const NavLinks = [
 ];
 
 const NavSection = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
     <div className="dark:bg-black/50 backdrop-blur-sm sticky top-0 z-50">
-      <div className="h-16 container mx-auto px-4 flex justify-between items-center">
+      <div className="h-16 container mx-auto px-4 flex justify-between items-center relative">
         <div className=" font-bold text-xl text-primary">Brapurple</div>
         <nav className="hidden md:flex space-x-4 md:space-x-8 lg:space-x-12">
           {NavLinks.map((item, idx) => {
@@ -22,7 +31,23 @@ const NavSection = () => {
             );
           })}
         </nav>
-        <Button className="hover:text-purple-400">Get Exclusive</Button>
+        {menuOpen && (
+          <nav className="md:hidden absolute top-20 right-10 dark:bg-black/50 backdrop-blur-sm flex flex-col gap-2">
+            {NavLinks.map((item, idx) => {
+              return (
+                <Link key={idx} href={item.path}>
+                  {item.name}
+                </Link>
+              );
+            })}
+          </nav>
+        )}
+        <div onClick={toggleMenu} className="md:hidden text-primary">
+          {menuOpen ? <X /> : <AlignJustify />}
+        </div>
+        <Button className="hover:text-purple-400 hidden md:block">
+          Get Exclusive
+        </Button>
       </div>
     </div>
   );
